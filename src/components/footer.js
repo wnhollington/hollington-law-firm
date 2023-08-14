b42876ad75e4fdd584f6daa095fb7a15bd192e79
Neal Hollington <neal.hollington@gmail.com>
2023-08-14 00:29:14 -0600

    breakout navigation into desktop and mobile components

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
             <div class="flex flex-col items-center justify-center">
 
                 <ul className="flex my-4">
-                    <li className="text-sm md:text-lg text-gray-200 mx-2 transition-colors duration-300 hover:text-gray-300"><Link to="/privacy-policy">Privacy Policy</Link></li>
-                    <li className="text-sm md:text-lg text-gray-200 mx-2 transition-colors duration-300 hover:text-gray-300"><Link to="/contact">Contact</Link></li>
+                    <li className="text-sm md:text-lg font-semibold text-gray-200 mx-2 transition-colors duration-300 hover:text-gray-300"><Link to="/privacy-policy">Privacy Policy</Link></li>
+                    <li className="text-sm md:text-lg text-gray-200 font-semibold mx-2 transition-colors duration-300 hover:text-gray-300"><Link to="/contact">Contact</Link></li>
                 </ul>
                 <p class="text-sm text-gray-300">© Copyright 2023. All Rights Reserved.</p>
             </div>
