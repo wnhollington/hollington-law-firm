aa26467f3a9bc3c28c10fa60379c3e7039db51bf
Neal Hollington <neal.hollington@gmail.com>
2023-08-16 18:51:06 -0600

    add testimonial slider

--- src/components/sections/testimonials.js
+++ src/components/sections/testimonials.js
@@ -6,9 +6,7 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const Testimonials = () => {
     const testimonials = [
-        {id: 1, author: "Lauren Hollington", review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-        {id: 2, author: "Neal Hollington", review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-        {id: 3, author: "Bethany Pfeiffer", review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+        {id: 1, author: "Emilia Rodriguez", review: "I would like to appreciate all the hard work that you have put into completing my case. I am very grateful to you. Before I contacted you, I was hopeless. I have no words to express my gratitude to you."},
     ]
     const { ref, inView } = useInView({
         threshold: .1,
@@ -17,8 +15,8 @@ const Testimonials = () => {
     return (
         <section id="testimonials" ref={ref}>
             <div className={`container my-24 animated animatedFadeInUp ${inView ? 'fadeInUp' : null}`}>
-                <h2 className="text-center text-3xl font-semibold mb-2">What Our Clients are Saying</h2>
-                <div className="w-10 mx-auto border-b-4 border-blue-800 mb-8"></div>
+                <h2 className="text-center text-3xl font-semibold mb-2">Client Stories</h2>
+                <div className="w-10 mx-auto border-b-4 border-blue-900 mb-8"></div>
                 <div className="lg:flex items-center justify-center">
 
                     <div class="mt-8 lg:px-6 lg:mt-0">
@@ -34,8 +32,8 @@ const Testimonials = () => {
                                         return (
                                             <Slide index={1}>
                                                 <div className="items-center justify-center block">
-                                                    <p class="mt-6 text-gray-500 text-lg"><span className="text-5xl font-semibold text-blue-800 inline"><RiChatQuoteFill/></span>{testimonial.review}</p>
-                                                    <h3 class="mt-6 text-lg text-center font-medium text-blue-800">{testimonial.author}</h3>
+                                                    <p class="mt-6 text-gray-500 text-lg"><span className="text-5xl font-semibold text-blue-900 inline"><RiChatQuoteFill/></span>{testimonial.review}</p>
+                                                    <h3 class="mt-6 text-lg text-center font-medium text-blue-900">{testimonial.author}</h3>
                                                 </div>
                                             </Slide>
                                         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Hero from "../components/sections/hero"
 import Accolades from "../components/sections/accolades"
+import Testimonials from "../components/sections/testimonials"
 
 const IndexPage = () => {
   return (
     <Layout>
       <Hero />
       <Accolades />
+      <Testimonials />
     </Layout>
   )
 }
