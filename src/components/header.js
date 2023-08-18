26b80d8d55d76149023e2b707d0e6fbd4bebe195
Neal Hollington <neal.hollington@gmail.com>
2023-08-17 20:46:27 -0600

    remove fixed header on contact and consultation pages to fix glitch scroll

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,10 @@ const Header = () => {
     useEffect(() => {
         const handleScroll = () => {
             const isScrolled = window.scrollY > 50;
-            if (isScrolled !== scrolled) {
+            // Check if Url is on Contact or Schedule Pages
+            const url = typeof window !== 'undefined' ? window.location.href : ''
+            const urlCheck = url.includes("schedule-consultation") || url.includes("contact") ? true : false
+            if ((isScrolled !== scrolled) & (urlCheck === false)) {
                 setScrolled(!scrolled)
             }
         }
@@ -24,11 +27,12 @@ const Header = () => {
             document.removeEventListener('scroll', handleScroll)
         }
     }, [scrolled])
-
+    
     return (
         <header>
             <div className={`inset-x-0 top-0 z-50 bg-white shadow-lg transition-all ease-in-out delay-150 ${scrolled === false ? "relative" : "fixed"}`}>
                 <div className="flex justify-between items-center py-6 px-2">
+                    
                     <Link to="/"><StaticImage src="../images/logo-web.png"/></Link>
                     <Desktop/>
                     <Mobile/>
