ea4a598de4d167ee6f919cca2db3df96cc9b6d7b
Neal Hollington <neal.hollington@gmail.com>
2023-08-21 19:55:58 -0600

    move fslightbox to fix display issues

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -8,23 +8,24 @@ const Hero = () => {
     const siteMetaData = useSiteMetadata()
     const [toggler, setToggler] = useState(false)
     return (
-        <section className="relative isolate bg-hero bg-cover bg-no-repeat lg:h-screen">
-            <div className="h-full max-w-6xl mx-auto flex items-center justify-between p-2">
-                <div className="text-center">
-                    <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">{siteMetaData.title}</h1>
-                    <p className="mt-6 text-2xl leading-8">{siteMetaData.tagline}</p>
-                </div>
-                <div className="bg-profile bg-no-repeat w-96 h-96 lg:h-3/5 bg-cover flex items-center rounded-md">
-                    <BsPlayCircle size={64} className="text-primary animate-pulse mx-auto mt-48 hover:cursor-pointer" onClick={() => setToggler(!toggler)}/>
-                    <FsLightbox 
-                        toggler={toggler}
-                        className="z-50"
-                        sources={[
-                            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-                        ]}
-                    />
+        <section>
+            <div className="relative isolate bg-hero bg-cover bg-no-repeat lg:h-screen">
+                <div className="h-full max-w-6xl mx-auto flex items-center justify-between p-2">
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">{siteMetaData.title}</h1>
+                        <p className="mt-6 text-2xl leading-8">{siteMetaData.tagline}</p>
+                    </div>
+                    <div className="bg-profile bg-no-repeat w-96 h-96 lg:h-3/5 bg-cover flex items-center rounded-md">
+                        <BsPlayCircle size={64} className="text-primary animate-pulse mx-auto mt-48 hover:cursor-pointer" onClick={() => setToggler(!toggler)}/>
+                    </div>
                 </div>
             </div>
+            <FsLightbox 
+                toggler={toggler}
+                sources={[
+                    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                ]}
+            />
         </section>
     )
 }
