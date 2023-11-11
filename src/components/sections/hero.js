58c1a6923786a2b0c07d5355806751f81fbbe4e8
wnhollington <wnhollington@outlook.com>
2023-11-11 10:05:27 -0700

    remove hero portrait photo on mobile

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -11,19 +11,18 @@ const Hero = () => {
     const [toggler, setToggler] = useState(false)
     return (
         <section>
-            <div className="relative isolate bg-hero bg-cover bg-no-repeat xl:h-screen">
-                <div className="h-full max-w-full flex flex-col-reverse sm:flex-row items-center md:justify-around p-2 md:gap-10 bg-red-100/60">
+            <div className="relative isolate bg-hero bg-cover bg-no-repeat h-96 md:h-auto xl:h-screen">
+                <div className="h-full max-w-full flex flex-col-reverse sm:flex-row items-center justify-center md:justify-around p-2 md:gap-10 bg-red-100/60">
                     <div className="xl:text-center my-4">
                         <h1 className="font-['Libre_Baskerville'] text-3xl md:text-4xl lg:text-5xl max-w-2xl">{siteMetaData.tagline}</h1>
-                        <div className="flex flex-row my-2 md:my-6">
+                        <div className="flex flex-row my-2 mx-auto md:my-6">
                             <Link to="/schedule-consultation" className="bg-primary hover:shadow-lg text-white text-md md:text-lg font-bold p-4 rounded-lg">Schedule Your Free Consultation</Link>
                             <BsPlayCircle size={64} className="text-primary animate-pulse hover:cursor-pointer mx-auto" onClick={() => setToggler(!toggler)}/>
                         </div>
                     </div>
-                    <div className="flex flex-col justify-center">
+                    <div className="hidden md:block">
                         <StaticImage 
-                            src="https://res.cloudinary.com/wnhollington/image/upload/e_improve:outdoor,g_face/e1eqadzarq4bjsapvwgy.jpg"
-                            className="relative"
+                            src="https://res.cloudinary.com/wnhollington/image/upload/e_improve:outdoor:100/q_60/e1eqadzarq4bjsapvwgy.jpg"
                             alt="Altitude Injury Law"
                         />
                     </div>
