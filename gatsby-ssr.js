0ca18c58e2cd28293483890b025e0b7a54dfa061
wnhollington <wnhollington@outlook.com>
2024-03-02 16:12:55 -0700

    add GTM to site

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,12 +4,25 @@ import { Partytown } from '@builder.io/partytown/react';
 // Party Town, G-Tag
 const ORIGIN = 'https://www.googletagmanager.com';
 
-export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+export const onRenderBody = ({ setHeadComponents, setPreBodyComponents, setHtmlAttributes }) => {
   if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') return null;
 
   setHtmlAttributes({ lang: 'en-US' });
   setHeadComponents([
     <Partytown key="partytown" forward={['gtag']} />,
+    // GTM
+    <script
+      key="google-tag-manager"
+      type="text/partytown"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GTM_ID}');`
+      }}
+    />,
+    // Google Analytics
     <script key="google-analytics" type="text/partytown" src={`${ORIGIN}/gtag/js?id=${process.env.GA_TRACKING_ID}`} />,
     <script
       key="google-analytics-config"
@@ -22,6 +35,16 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
       }}
     />
   ]);
+  setPreBodyComponents([
+    <noscript
+      key="google-tag-manager"
+      dangerouslySetInnerHTML={{
+        __html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                `,
+      }}
+    />
+  ])
 };
   
     
