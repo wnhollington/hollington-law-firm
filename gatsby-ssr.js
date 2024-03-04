71b93095fca891998a944b2641c2dc0cfe51df07
wnhollington <wnhollington@outlook.com>
2024-03-04 13:38:09 -0700

    revise gtm script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -23,7 +23,6 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents, setHtmlA
   ]);
   setPreBodyComponents([
     <noscript
-      key="google-tag-manager"
       dangerouslySetInnerHTML={{
         __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
       }}
