9152a80383a1de65fd5d9376c6b552ad3452522a
wnhollington <wnhollington@outlook.com>
2024-03-10 14:20:01 -0600

    revise thank you page

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -4,20 +4,19 @@ import { BsCheck2Circle } from "react-icons/bs";
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const Contact = () => (
+const ThankYou = () => (
   <Layout>
     <div className="w-full flex flex-col items-center my-4 py-4 h-screen overflow-hidden">
-      <h1 className="max-w-lg mb-4 text-3xl leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center">Get in Touch</h1>
       <div className="mt-12 flex flex-col gap-4 justify-center items-center mx-8">
         <BsCheck2Circle size={80} color="#3C8D2F"/>
-        <p className="text-3xl font-medium text-gray-900">Thank You!</p>
+        <h1 className="text-3xl font-medium text-gray-900">Thank You For Reaching Out!</h1>
         <p className="text-xl font-medium text-gray-900">Your form submission has been sent and received. A representative will be in touch shortly.</p>
       </div>
     </div>   
   </Layout>
 )
 
-export default Contact
+export default ThankYou
 
 export const Head = () => (
   <Seo 
