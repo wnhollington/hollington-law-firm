f817172b3ea1a6020448062d391728243e6870ac
wnhollington <wnhollington@outlook.com>
2024-04-01 08:01:18 -0600

    revise practice areas main page section to include only two columns

--- src/components/sections/practice-areas.js
+++ src/components/sections/practice-areas.js
@@ -29,7 +29,7 @@ const PracticeAreas = () => {
             <div className={`container my-16 lg:my-24 text-center animated animatedFadeInUp ${inView ? 'fadeInUp' : null}`}>
                 <h2 className="text-center text-3xl font-medium mb-2">Cases We Handle</h2>
                 <div className="w-10 mx-auto border-b-4 border-primary mb-8"></div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     {data.allContentfulPracticeAreas.edges.map((edge, index) => {
                         return (
                             <Link 
