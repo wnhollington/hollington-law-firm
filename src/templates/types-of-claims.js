ef41580860558c783a47952656a4541ef228cb69
wnhollington <wnhollington@outlook.com>
2024-06-10 16:19:36 -0600

    revise entry links for project and claim templates

--- src/templates/types-of-claims.js
+++ src/templates/types-of-claims.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { INLINES } from '@contentful/rich-text-types';
+import { Link } from 'gatsby';
 
 // Components
 import Layout from '../components/layout.js'
@@ -9,13 +11,32 @@ import Sidebar from '../components/sidebar/index.js'
 
 // Render
 function TypesOfProjects({ data }) {
-
+  const options = {
+    renderNode: {
+      [INLINES.HYPERLINK]: (node) => {
+       return <a href={node.data.uri} target={`${node.data.uri.startsWith('https://hollingtonlawfirm.') ? '_self' : '_blank'}`}>{node.content[0].value}</a>;
+      },
+      [INLINES.ENTRY_HYPERLINK]: (node) => {
+        const entry = data.contentfulTypesOfClaims.body.references.find(x => x.contentful_id === node.data.target.sys.id)
+        switch (entry.internal.type) {
+          case 'ContentfulTypesOfClaims':
+            return <Link to={`/types-of-claims/${entry.slug}`}>{node.content[0].value}</Link>;
+          case 'ContentfulAttorneys':
+            return <Link to={`/${entry.slug}`}>{node.content[0].value}</Link>;
+          case 'ContentfulArticles':
+            return <Link to={`/articles/${entry.slug}`}>{node.content[0].value}</Link>;
+          default:
+            return <Link to={`/`}>{node.content[0].value}</Link>;
+        }
+      }
+    }
+  }
   return (
     <Layout>
       <div className='flex flex-col lg:flex-row my-8 p-4 gap-6 justify-center'>
         <article className='lg:w-2/3 max-w-6xl mx-auto'>
           <h1 className="bg-gradient-to-b from-primary to-red-800 text-center text-white mb-2 py-8 px-2 rounded-md shadow-xl">{data.contentfulTypesOfClaims.title}</h1>
-          <div>{renderRichText(data.contentfulTypesOfClaims.body)}</div>
+          <div>{renderRichText(data.contentfulTypesOfClaims.body, options)}</div>
         </article>
         <Sidebar />
       </div>
@@ -33,6 +54,15 @@ export const query = graphql`
       slug
       body {
         raw
+        references {
+          ... on ContentfulAttorneys {
+            contentful_id
+            slug
+            internal {
+              type
+            }
+          }
+        }
       }
     }
   }
--- src/templates/types-of-projects.js
+++ src/templates/types-of-projects.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { INLINES } from '@contentful/rich-text-types';
 
 // Components
 import Layout from '../components/layout.js'
@@ -9,13 +10,19 @@ import Sidebar from '../components/sidebar/index.js'
 
 // Render
 function TypesOfProjects({ data }) {
-
+  const options = {
+    renderNode: {
+      [INLINES.HYPERLINK]: (node) => {
+       return <a href={node.data.uri} target={`${node.data.uri.startsWith('https://hollingtonlawfirm.') ? '_self' : '_blank'}`}>{node.content[0].value}</a>;
+      },
+    }
+  }
   return (
     <Layout>
       <div className='flex flex-col lg:flex-row my-8 p-4 gap-6 justify-center'>
         <article className='lg:w-2/3 max-w-6xl mx-auto'>
           <h1 className="bg-gradient-to-b from-primary to-red-800 text-center text-white mb-2 py-8 px-2 rounded-md shadow-xl">{data.contentfulTypesOfProjects.title}</h1>
-          <div>{renderRichText(data.contentfulTypesOfProjects.body)}</div>
+          <div>{renderRichText(data.contentfulTypesOfProjects.body, options)}</div>
         </article>
         <Sidebar />
       </div>
