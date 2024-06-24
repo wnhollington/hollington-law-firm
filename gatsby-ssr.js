535969daa05a582cf7e0fb8ffce0da393edd68d7
wnhollington <wnhollington@outlook.com>
2024-06-24 12:15:16 -0600

    add calendly event listener

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,6 +10,22 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents, setHtmlA
       dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GTM_ID}');`
       }}
+    />,
+    <script 
+      dangerouslySetInnerHTML={{
+        __html: `
+          function isCalendlyEvent(e) {
+            return e.data.event &&
+            e.data.event.indexOf('calendly') === 0;
+          };
+          window.addEventListener('message',function(e) {
+            if (isCalendlyEvent(e)) {
+              //console.log(e.data);
+              dataLayer.push(e.data);
+            }
+          }
+        `
+      }}
     />
   ]);
   setPreBodyComponents([
