bad98b16a19bd58f95a7426e4a2e62f7ead534c1
wnhollington <wnhollington@outlook.com>
2024-06-24 11:40:51 -0600

    update ssr for google tag script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,19 +7,17 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents, setHtmlA
   setHtmlAttributes({ lang: 'en-US' });
 
   setHeadComponents([
-    // Partytown/GTM
-    <Partytown key="partytown" debug={true} forward={['dataLayer.push']} />,
-    <script
-      key="google-tag-manager"
-      type='text/partytown'
+    <script type="text/partytown" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}></script>,
+    <script 
+      type="text/partytown"
       dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.GTM_ID}');`
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          window.gtag = function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GTM_ID}');`
       }}
-    />,
+    />
   ]);
   setPreBodyComponents([
     <noscript
