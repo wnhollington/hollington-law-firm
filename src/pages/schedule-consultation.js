24e6f9ed8fe6c11af1a3a60ce006ab482456736b
wnhollington <wnhollington@outlook.com>
2024-06-24 13:06:53 -0600

    update calendly colors

--- src/pages/schedule-consultation.js
+++ src/pages/schedule-consultation.js
@@ -8,7 +8,7 @@ const ScheduleConsultation = () => (
   <Layout>
     <div className="my-4 py-4 2xl:h-screen overflow-hidden">
       <h1 className="mb-4 text-3xl leading-none tracking-tight text-gray-900 sm:text-4xl mx-auto text-center">Schedule Consultation</h1>
-      <InlineWidget url="https://calendly.com/wnhollington" />
+      <InlineWidget url="https://calendly.com/wnhollington??primary_color=0c428d" />
     </div>
   </Layout>
 )
