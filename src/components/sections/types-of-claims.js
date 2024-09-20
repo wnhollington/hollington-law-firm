5f2fe5cfcf1111cefa0199ccfbc5252c277d8470
wnhollington <wnhollington@outlook.com>
2024-09-19 19:20:29 -0600

    add view all and links to claims handled section on index page

--- src/components/sections/types-of-claims.js
+++ src/components/sections/types-of-claims.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { useInView } from "react-intersection-observer"
+import { Link } from "gatsby"
 
 const TypesOfClaims = () => {
     const { ref, inView } = useInView({
@@ -7,23 +8,27 @@ const TypesOfClaims = () => {
         triggerOnce: true
     })
 
-    const typesOfDefects = ['Design Defects', 'Material Defects', 'Expansive Soils', 'Poor Workmanship', 'Insurance Claims', 'Nondisclosure & Fraud']
-
     return (
         <section id="types-of-claims" ref={ref}>
             <div className={`container my-16 lg:my-24 text-center animated animatedFadeInUp ${inView ? 'fadeInUp' : null}`}>
                 <h2 className="text-center text-3xl font-medium mb-2">Claims We Handle</h2>
                 <div className="w-10 mx-auto border-b-4 border-primary mb-8"></div>
-                <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                    {typesOfDefects.map((edge, index) => {
-                        return (
-                            <li
-                            key={index}
-                            className="bg-gradient-to-b from-primary to-red-800 hover:shadow-lg text-white text-lg md:text-2xl font-semibold p-4 rounded-md"
-                            >{edge}</li>                         
-                        )
-                    })}
-                </ul>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-12">
+
+                    <Link to="/types-of-claims/design-defects" className="bg-gradient-to-b from-primary to-red-800 hover:shadow-lg text-white text-lg md:text-2xl font-semibold p-4 rounded-md">Design Defects</Link>
+
+                    <Link to="/types-of-claims/material-defects" className="bg-gradient-to-b from-primary to-red-800 hover:shadow-lg text-white text-lg md:text-2xl font-semibold p-4 rounded-md">Material Defects</Link>
+
+                    <Link to="/types-of-claims/expansive-soils" className="bg-gradient-to-b from-primary to-red-800 hover:shadow-lg text-white text-lg md:text-2xl font-semibold p-4 rounded-md">Expansive Soils</Link>
+                    
+                    <Link to="/types-of-claims/poor-workmanship" className="bg-gradient-to-b from-primary to-red-800 hover:shadow-lg text-white text-lg md:text-2xl font-semibold p-4 rounded-md">Poor Workmanship</Link>
+                    
+                    <Link to="/types-of-claims/insurance-claim-lawyer" className="bg-gradient-to-b from-primary to-red-800 hover:shadow-lg text-white text-lg md:text-2xl font-semibold p-4 rounded-md">Insurance Claims</Link>
+
+                    <Link to="/types-of-claims/nondisclosure-and-fraud" className="bg-gradient-to-b from-primary to-red-800 hover:shadow-lg text-white text-lg md:text-2xl font-semibold p-4 rounded-md">Nondisclosure & Fraud</Link>           
+
+                </div>
+                <Link to="/types-of-claims" className="bg-gradient-to-b from-primary to-red-800 hover:shadow-lg text-white text-lg md:text-2xl font-semibold p-4 rounded-md">View All</Link>
             </div>
         </section>
     )
