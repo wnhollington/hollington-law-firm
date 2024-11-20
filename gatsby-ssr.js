c7bf577d19df4da620bb42e03a5df639546bf382
wnhollington <wnhollington@outlook.com>
2024-11-19 18:54:25 -0700

    add lawmatics tracking

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,6 +10,11 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents, setHtmlA
       dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GTM_ID}');`
       }}
+    />,
+    <script 
+      dangerouslySetInnerHTML={{
+        __html: `!function(e,t,n,a,i,s,c,o,l){e[i]||(c=e[i]=function(){c.process?c.process.apply(c,arguments):c.queue.push(arguments)},c.queue=[],c.t=1*new Date,o=t.createElement(n),o.async=1,o.src=a+'?t='+Math.ceil(new Date/s)*s,l=t.getElementsByTagName(n)[0],l.parentNode.insertBefore(o,l))}(window,document,'script','https://navi.lawmatics.com/navi.min.js','lm_navi',864e5),lm_navi('init','${process.env.LAWMATICS_ID}'),lm_navi('event','pageload')`
+      }}
     />
   ]);
   setPreBodyComponents([
