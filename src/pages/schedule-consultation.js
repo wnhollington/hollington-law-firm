ca78735909f5f93b680e5617fe4a8fe6b05aa7e5
wnhollington <wnhollington@outlook.com>
2024-11-21 19:03:20 -0700

    incorporate lawmatics forms into website

--- src/pages/schedule-consultation.js
+++ src/pages/schedule-consultation.js
@@ -1,17 +1,29 @@
 import * as React from "react"
-import {InlineWidget} from "react-calendly"
+import { useEffect } from "react"
+
 // Components
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const ScheduleConsultation = () => (
-  <Layout>
-    <div className="my-4 py-4 2xl:h-screen overflow-hidden">
-      <h1 className="mb-4 text-3xl leading-none tracking-tight text-gray-900 sm:text-4xl mx-auto text-center">Schedule Consultation</h1>
-      <InlineWidget url="https://calendly.com/wnhollington?primary_color=6E0A05" />
-    </div>
-  </Layout>
-)
+const ScheduleConsultation = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.id = "lm-embedded-script";
+    script.async = true;
+    script.innerHTML = `
+      !function(e,t,n,a,s,c,i){if(!e[s]){i=e[s]=function(){i.process?i.process.apply(i,arguments):i.queue.push(arguments)},i.queue=[],i.t=1*new Date;var o=t.createElement(n);o.async=1,o.src=a+"?t="+Math.ceil(new Date/c)*c;var r=t.getElementsByTagName(n)[0];r.parentNode.insertBefore(o,r)}}(window,document,"script","https://navi.lawmatics.com/intake.min.js","lm_intake",864e5),lm_intake("be59a2ca-303f-45ac-86cd-f57999971623");
+    `;
+    document.body.appendChild(script);
+  }, []);
+  return (
+    <Layout>
+      <div className="my-4 py-4 min-h-screen">
+        <h1 className="mb-12 text-3xl leading-none tracking-tight text-gray-900 sm:text-4xl mx-auto text-center">Schedule Consultation</h1>
+        <div id="lm-embedded-script"></div>
+      </div>
+    </Layout>
+  )
+}
 export default ScheduleConsultation
 
 export const Head = () => (
