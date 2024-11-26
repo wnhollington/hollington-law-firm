6baf93979d12ca24f93d116caaafa96ca65a2f8c
wnhollington <wnhollington@outlook.com>
2024-11-25 17:26:07 -0700

    revise scheduling contact form to avoid duplicate header in redirect

--- src/pages/schedule-consultation.js
+++ src/pages/schedule-consultation.js
@@ -11,13 +11,13 @@ const ScheduleConsultation = () => {
     script.id = "lm-embedded-script";
     script.async = true;
     script.innerHTML = `
-      !function(e,t,n,a,s,c,i){if(!e[s]){i=e[s]=function(){i.process?i.process.apply(i,arguments):i.queue.push(arguments)},i.queue=[],i.t=1*new Date;var o=t.createElement(n);o.async=1,o.src=a+"?t="+Math.ceil(new Date/c)*c;var r=t.getElementsByTagName(n)[0];r.parentNode.insertBefore(o,r)}}(window,document,"script","https://navi.lawmatics.com/intake.min.js","lm_intake",864e5),lm_intake("be59a2ca-303f-45ac-86cd-f57999971623");
+      !function(e,t,n,a,s,c,i){if(!e[s]){i=e[s]=function(){i.process?i.process.apply(i,arguments):i.queue.push(arguments)},i.queue=[],i.t=1*new Date;var o=t.createElement(n);o.async=1,o.src=a+"?t="+Math.ceil(new Date/c)*c;var r=t.getElementsByTagName(n)[0];r.parentNode.insertBefore(o,r)}}(window,document,"script","https://navi.lawmatics.com/intake.min.js","lm_intake",864e5),lm_intake("862e0bf9-7192-434f-a858-5bb13faf312d");
     `;
     document.body.appendChild(script);
   }, []);
   return (
     <Layout>
-      <div className="my-4 py-4 min-h-screen">
+      <div className="my-4 py-4 min-h-screen" id="scheduling-page">
         <h1 className="mb-12 text-3xl leading-none tracking-tight text-gray-900 sm:text-4xl mx-auto text-center">Schedule Consultation</h1>
         <div id="lm-embedded-script"></div>
       </div>
