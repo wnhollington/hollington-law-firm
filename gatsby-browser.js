57846bf2b4f0be2a1c523ae279317fcff2d45779
wnhollington <wnhollington@outlook.com>
2025-05-13 11:17:22 -0600

    omit unused fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,14 @@
 import "./src/styles/global.css";
-import "@fontsource/libre-baskerville";
-import "@fontsource/source-sans-pro";
+
+// Libre Baskerville
+import "@fontsource/libre-baskerville/400.css";
+import "@fontsource/libre-baskerville/400-italic.css";
+
+// Source Sans Pro
+import "@fontsource/source-sans-pro/400.css";
+import "@fontsource/source-sans-pro/400-italic.css";
+import "@fontsource/source-sans-pro/700.css";
+import "@fontsource/source-sans-pro/700-italic.css";
 
 /**
  * Loads the Lawmatics tracking pixel on every route.
