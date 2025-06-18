904fc42e120bcc90e3edaef9effc277e8b0ee006
wnhollington <wnhollington@outlook.com>
2025-06-18 07:36:57 -0600

    move GTM to ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,6 +8,9 @@ export const onRenderBody = ({
   setHtmlAttributes({ lang: "en-US" });
 
   const GTM_ID = process.env.GTM_ID || "";
+  const isProduction = process.env.NODE_ENV === "production";
+
+  if (!isProduction || !GTM_ID) return;
 
   setHeadComponents([
     <link
@@ -15,6 +18,17 @@ export const onRenderBody = ({
       rel="preconnect"
       href="https://www.googletagmanager.com"
       crossOrigin="anonymous"
+    />,
+    <script
+      key="gtm-inline-script"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');`,
+      }}
     />,
   ]);
 
