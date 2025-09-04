2093f65a93c7d4052ae3483dd867a93b142f86d3
wnhollington <wnhollington@outlook.com>
2025-09-04 14:49:42 -0600

    add google search console meta description for seo team to gatsby-ssr.js file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,4 @@
+// gatsby-ssr.js
 import React from "react";
 
 export const onRenderBody = ({
@@ -7,10 +8,20 @@ export const onRenderBody = ({
 }) => {
   setHtmlAttributes({ lang: "en-US" });
 
+  // Always include Google Search Console verification
+  setHeadComponents([
+    <meta
+      key="gsc-verification"
+      name="google-site-verification"
+      content="Fk-brWpmhcQzt8X8qBRODdkLj1jVK4zc7l8MYIVWBoA"
+    />,
+  ]);
+
   const GTM_ID = process.env.GTM_ID || "";
   const isProduction = process.env.NODE_ENV === "production";
 
-  if (!isProduction || !GTM_ID) return;
+  // Only add GTM in production with an ID
+  if (!(isProduction && GTM_ID)) return;
 
   setHeadComponents([
     <link
@@ -23,11 +34,11 @@ export const onRenderBody = ({
       key="gtm-inline-script"
       dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
-          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');`,
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
       }}
     />,
   ]);
